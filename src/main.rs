0a27e6e317e6565e605607a4a8479a4c6487388d
Thomas Harris <teh019283@gmail.com>
2020-03-30 20:02:15 +0200

    Pull out the peer into an async function, make work queue static
    Not the ideal solution honestly

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use rand::Rng;
 use structopt::StructOpt;
 
 use crate::bencode::de::deserialize;
+use async_std::sync::{Arc, Mutex};
 use crossbeam::queue::SegQueue;
+use flume::Sender;
+use lazy_static::lazy_static;
 
 mod bencode;
 
@@ -13,6 +16,10 @@ mod bencode;
 struct Args {
     #[structopt(parse(from_os_str))]
     torrent_file_path: PathBuf,
+}
+
+lazy_static! {
+    static ref WORK_QUEUE: SegQueue<i32> = SegQueue::new();
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -31,18 +38,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("woah async");
         async_std::task::spawn(async {
             println!("time to nest");
-        }).await;
+        })
+            .await;
     });
-
-    let work_queue: async_std::sync::Arc<async_std::sync::Mutex<SegQueue<i32>>> =
-        async_std::sync::Arc::new(async_std::sync::Mutex::new(SegQueue::new()));
 
     let (done_tx, mut done_rx) = flume::unbounded::<i32>();
 
     async_std::task::block_on(async {
-        let q = work_queue.lock().await;
         for i in 0..10000 {
-            q.push(i)
+            WORK_QUEUE.push(i)
         }
     });
 
@@ -53,36 +57,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     async_std::task::block_on(async move {
         let mut children = vec![];
         for i in 0..5i32 {
-            let q = work_queue.clone();
             let c = counter.clone();
             let d = done_tx.clone();
-            children.push(async_std::task::spawn(async move {
-                println!("Starting {}", i);
-                loop {
-                    let x = {
-                        let l = q.lock().await;
-                        match l.pop() {
-                            Ok(x) => x,
-                            _ => {
-                                break;
-                            }
-                        }
-                    };
-                    let sleep_t = (i + 1) * 2;
-                    println!("{}: Found {}. sleeping for {}", i, x, sleep_t);
-                    // tweak from_x here to see clear effects on task distribution
-                    async_std::task::sleep(std::time::Duration::from_millis(sleep_t as u64)).await;
-                    if x.abs() % 2 != i % 2 {
-                        let l = q.lock().await;
-                        println!("{}: pushing {}", i, x - 1);
-                        l.push(x - 1);
-                    } else {
-                        d.send(x).unwrap();
-                        c.lock().await[i as usize] += 1;
-                    }
-                }
-                println!("Done {}", i);
-            }));
+            children.push(async_std::task::spawn(peer(i, &WORK_QUEUE, d, c)));
         }
 
         drop(done_tx);
@@ -105,7 +82,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         futures::future::join_all(children).await;
 
-        println!("Counter: {:?}", counter.lock().await);
+        println!("Counter: {:?} WORK_QUEUE.len(): {}", counter.lock().await, WORK_QUEUE.len());
     });
 
     Ok(())
@@ -122,4 +99,35 @@ fn gen_peer_id() -> [u8; 20] {
     let mut res = [0u8; 20];
     res.copy_from_slice(&id);
     res
+}
+
+async fn peer(
+    i: i32,
+    q: &SegQueue<i32>,
+    d: Sender<i32>,
+    c: Arc<Mutex<[u32; 5]>>,
+) -> () {
+    println!("Starting {}", i);
+    loop {
+        let x = {
+            match q.pop() {
+                Ok(x) => x,
+                _ => {
+                    break;
+                }
+            }
+        };
+        let sleep_t = (i + 1) * 2;
+        println!("{}: Found {}. sleeping for {}", i, x, sleep_t);
+        // tweak from_x here to see clear effects on task distribution
+        async_std::task::sleep(std::time::Duration::from_millis(sleep_t as u64)).await;
+        if x.abs() % 2 != i % 2 {
+            println!("{}: pushing {}", i, x - 1);
+            q.push(x - 1);
+        } else {
+            d.send(x).unwrap();
+            c.lock().await[i as usize] += 1;
+        }
+    }
+    println!("Done {}", i);
 }
