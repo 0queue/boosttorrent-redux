0814b4320d2f14f4053b70057f78f92edb6c6ccd
Thomas Harris <teh019283@gmail.com>
2020-04-23 22:17:47 +0200

    Implement the controller

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -2,6 +2,11 @@ use async_std::sync::Arc;
 use async_std::sync::RwLock;
 use crate::PieceMeta;
 use bit_vec::BitVec;
+use async_std::fs::File;
+use flume::RecvError;
+use util::ext::bitvec::BitVecExt;
+use futures::{AsyncSeekExt, AsyncWriteExt};
+use futures::io::SeekFrom;
 
 #[derive(PartialEq, Copy, Clone)]
 #[allow(dead_code)]
@@ -41,6 +46,60 @@ pub struct State {
 
 pub struct TorrentInfo {
     pub pieces: Vec<PieceMeta>,
+    pub piece_length: usize,
     pub id: [u8; 20],
     pub file_hash: [u8; 20],
+}
+
+pub async fn controller(
+    mut output_file: File,
+    torrent_info: TorrentInfo,
+    mut done_rx: flume::Receiver<DownloadedPiece>,
+    work_rx: async_std::sync::Receiver<usize>,
+    controller_state: ControllerState,
+) {
+    let mut local_bitfield = controller_state.read().await.bitfield.clone();
+
+    loop {
+        match done_rx.recv_async().await {
+            Err(_) => {
+                println!("all senders disconnected");
+                break;
+            }
+            Ok(piece) => if !local_bitfield[piece.index] {
+                // write to file
+                output_file
+                    .seek(SeekFrom::Start((piece.index * torrent_info.piece_length) as u64))
+                    .await
+                    .unwrap();
+                output_file.write_all(&piece.data).await.unwrap();
+
+                // update state
+                {
+                    local_bitfield.set(piece.index, true);
+                    let mut write = controller_state.write().await;
+                    write.bitfield.set(piece.index, true);
+                    write.haves.push(piece.index);
+                    if write.bitfield.all() {
+                        write.lifecycle = Lifecycle::Done;
+                    } else if work_rx.len() < 10 && write.lifecycle == Lifecycle::Downloading {
+                        write.lifecycle = Lifecycle::Endgame;
+                        println!("ENDGAME TRIGGERED");
+                        println!("> work_rx.len(): {}", work_rx.len());
+                        println!("> zeroes: {:?}", write.bitfield.zeroes());
+                    }
+                }
+
+                // report
+                let ones = local_bitfield.ones().len();
+                let percent = ones as f32 / torrent_info.pieces.len() as f32;
+                println!(
+                    "Finished with {:?}. Completed: {} / {} = {:.2}%",
+                    piece.index, ones, torrent_info.pieces.len(), percent * 100f32
+                )
+            },
+        }
+    }
+
+    println!("Done.  Zeroes: {:?}", local_bitfield.zeroes());
 }
